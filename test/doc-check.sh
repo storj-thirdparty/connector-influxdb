ad2980b282dd149798f110c745b7892468192bc5
Karl Mozurkewich <karl@utropicmedia.com>
2020-05-20 16:21:30 -0400

    Update doc-check.sh

--- test/doc-check.sh
+++ test/doc-check.sh
@@ -6,5 +6,5 @@ if [ -f $HOME_MD ] && [ -f $README_MD ]; then
     echo "doco check PASSED"
     exit 0
 fi
-echo "doco check FAILED"
+echo "doco check FAILED.  Missing either" $README_MD "or" $HOME_MD
 exit 1
