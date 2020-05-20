ba093469c94cef23e9c050a80a671ed3b3f983b8
Karl Mozurkewich <karl@utropicmedia.com>
2020-05-20 14:44:50 -0400

    Update doc-check.sh

--- test/doc-check.sh
+++ test/doc-check.sh
@@ -1,7 +1,9 @@
-pwd
-if [[ -f "./README.assets/Home.md" ]]; then
+## verify documentation is present. 
+## cd is /workspace
+$HOME_MD = "./README.assets/Home.md"
+$README_MD = "./README.md"
+if [[ -f $HOME_MD ] && [ -f $README_MD]]; then
     echo "yes"
-
     exit 0
 fi
 echo "no"
